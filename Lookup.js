200f46679a6bd9805e86cf630f1f2975c0f38428
Develifture <kurd.uk.me@gmail.com>
2024-11-29 00:52:43 +0000

    Lookup.js

--- Lookup.js
+++ Lookup.js
@@ -12,7 +12,7 @@ Usage Statement: ⚠️This script is for learning and communication purposes on
 
 [rewrite_local]
 
-https://buy.itunes.apple.com/verifyReceipt url script-response-body https://raw.githubusercontent.com/Develifture/codebreaker/refs/heads/main/Lookup.js?token=GHSAT0AAAAAACZDS3LXJXVSLIKUACY6J6ZGZ2JB4QQ
+https://buy.itunes.apple.com/verifyReceipt url script-response-body https://raw.githubusercontent.com/Develifture/codebreaker/refs/heads/1st/Loo.js?token=GHSAT0AAAAAACZDS3LXSHW2E3Y3Z2RCBJVWZ2JCB2A
 [mitm] 
 
 hostname = buy.itunes.apple.com
