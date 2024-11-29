0bc47d2cf666e22793d678dc5abd22eb06761279
Develifture <kurd.uk.me@gmail.com>
2024-11-29 01:17:01 +0000

    Update Lookup.js

--- Lookup.js
+++ Lookup.js
@@ -12,7 +12,7 @@ Usage Statement: ⚠️This script is for learning and communication purposes on
 
 [rewrite_local]
 
-https://buy.itunes.apple.com/verifyReceipt url script-response-body 
+https://buy.itunes.apple.com/verifyReceipt url script-response-body https://raw.githubusercontent.com/Develifture/codebreaker/refs/heads/main/LookupDict.js?token=GHSAT0AAAAAAC3FT64AYZ64QBRXXJJO3UB6Z2JCYQQ
 
 [mitm] 
 
